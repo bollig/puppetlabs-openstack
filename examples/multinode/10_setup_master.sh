e999c5c0e5eb688c1bf82d3375664ef6f6e4d298
Evan Bollig <bollig@gmail.com>
2016-02-08 11:37:32 -0600

    Working liberty builds

--- examples/multinode/10_setup_master.sh
+++ examples/multinode/10_setup_master.sh
@@ -8,9 +8,13 @@ sudo apt-get install -y puppetmaster puppet git; \
 sudo apt-get upgrade -y; \
 sudo service puppetmaster restart; \
 sudo puppet agent --enable; \
+sudo git clone https://github.com/bollig/puppetlabs-openstack.git -b liberty /opt/puppetlabs-openstack
+cd /opt/puppetlabs-openstack && sudo puppet module build; \
+cd /opt/puppetlabs-openstack/pkg && sudo puppet module install ./puppetlabs-openstack*.tar.gz; \
 sudo puppet agent -t;"
 
 
 
 #sudo rmdir /etc/puppet/modules || sudo unlink /etc/puppet/modules; \
 #sudo ln -sf /vagrant/modules /etc/puppet/modules; \
+#sudo ln -sf /vagrant/site.pp /etc/puppet/manifests/site.pp; \
