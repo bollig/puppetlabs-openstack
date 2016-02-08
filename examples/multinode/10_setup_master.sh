493e3a1596a06c3fd8960d04b62a97a9c4090029
Evan Bollig <bollig@gmail.com>
2016-02-08 12:07:26 -0600

    Expanded list of required modules

--- examples/multinode/10_setup_master.sh
+++ examples/multinode/10_setup_master.sh
@@ -8,9 +8,6 @@ sudo apt-get install -y puppetmaster puppet git; \
 sudo apt-get upgrade -y; \
 sudo service puppetmaster restart; \
 sudo puppet agent --enable; \
-sudo git clone https://github.com/bollig/puppetlabs-openstack.git -b liberty /opt/puppetlabs-openstack
-cd /opt/puppetlabs-openstack && sudo puppet module build; \
-cd /opt/puppetlabs-openstack/pkg && sudo puppet module install ./puppetlabs-openstack*.tar.gz; \
 sudo puppet agent -t;"
 
 
--- examples/multinode/11_setup_openstack.sh
+++ examples/multinode/11_setup_openstack.sh
@@ -5,7 +5,9 @@ sudo mkdir -p /etc/puppet/hieradata; \
 sudo ln -s /openstack/examples/common.yaml /etc/puppet/hieradata/common.yaml; \
 sudo ln -sf /vagrant/site.pp /etc/puppet/manifests/site.pp; \
 cd /opt && sudo git clone https://github.com/bollig/puppetlabs-openstack.git -b liberty; \
+cd /opt/puppetlabs-openstack && sudo git pull origin liberty; \
 cd /opt/puppetlabs-openstack && sudo puppet module build; \
+sudo rm -r /etc/puppet/modules/*; \
 cd /opt/puppetlabs-openstack/pkg && sudo puppet module install ./puppetlabs-openstack*.tar.gz; \
 sudo service puppetmaster restart; \
 sudo puppet agent -t;"
