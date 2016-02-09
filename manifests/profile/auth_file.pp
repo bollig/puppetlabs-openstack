9b60b7a755f3c7264d9d7ba3eabb26ca3d1d23e2
Evan Bollig <bollig@gmail.com>
2016-02-09 16:46:04 -0600

    Added additional TODOs

--- manifests/profile/auth_file.pp
+++ manifests/profile/auth_file.pp
@@ -6,5 +6,9 @@ class openstack::profile::auth_file {
     auth_url    => "http://${::openstack::config::controller_address_api}:5000/v3/",
     #auth_url    => "http://${::openstack::config::controller_address_api}:5000",
     region_name => $::openstack::config::region,
+# TODO: update puppet-openstack_extras module to version that supports adding
+# v3 creds to /root/openrc (and creating additional openrc files for other
+# users). Missing in this version: OS_USER_DOMAIN_NAME and
+# OS_PROJECT_DOMAIN_NAME
   }
 }
