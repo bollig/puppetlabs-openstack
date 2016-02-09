c24185b993bd775d97150d2d109997667b2ac6a5
Evan Bollig <bollig@gmail.com>
2016-02-08 23:06:02 -0600

    Ensuring that we get the latest from RDO on Centos

--- examples/multinode/20_setup_nodes.sh
+++ examples/multinode/20_setup_nodes.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+CMDS="sudo yum update -y; \
+sudo yum install -y https://www.rdoproject.org/repos/rdo-release.rpm; \
+sudo yum update -y; \
+sudo yum update -y; \
+sudo yum remove -y firewalld; \
+sudo puppet agent -t;"
+
 # Connect the agents to the master
-vagrant ssh control -c "sudo yum update -y; sudo yum remove -y firewalld; sudo puppet agent -t;"
-vagrant ssh network -c "sudo yum update -y; sudo yum remove -y firewalld; sudo puppet agent -t;"
-vagrant ssh storage -c "sudo yum update -y; sudo yum remove -y firewalld; sudo puppet agent -t;"
-vagrant ssh compute -c "sudo yum update -y; sudo yum remove -y firewalld; sudo puppet agent -t;"
+#vagrant ssh control -c $CMDS
+vagrant ssh network -c $CMDS
+vagrant ssh storage -c $CMDS
+vagrant ssh compute -c $CMDS
 
 # sign the certs
 vagrant ssh puppet -c "sudo puppet cert sign --all"
