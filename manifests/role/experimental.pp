fa950d7d3a8b745931ba1b667f470202bc54ce92
Evan Bollig <bollig@gmail.com>
2016-02-17 16:58:21 -0600

    Fixed naming on experimental

--- manifests/role/experimental.pp
+++ manifests/role/experimental.pp
@@ -1,4 +1,4 @@
-class openstack::role::storage inherits ::openstack::role {
+class openstack::role::experimental inherits ::openstack::role {
   $node_type = "${node_type}|experimental"
   class { '::openstack::profile::trove::auth': }
   class { '::openstack::profile::trove::api': }
