b5d598f0eb89eab537029c72ed0fae0ac0306b2a
Evan Bollig <bollig@gmail.com>
2016-05-06 19:30:16 -0500

    Clean volumes

--- manifests/common/cinder.pp
+++ manifests/common/cinder.pp
@@ -31,41 +31,5 @@ class openstack::common::cinder {
     glance_api_servers => [ $::openstack::config::glance_api_servers ],
   }
 
-   $backend = 'rbd'
-   case $backend {
-    'iscsi': {
-      class { '::cinder::setup_test_volume':
-        size => '1G',
-      }
-      cinder::backend::iscsi { 'BACKEND_1':
-        iscsi_ip_address => '127.0.0.1',
-      }
-    }
-    'rbd': {
-      cinder::backend::rbd { 'BACKEND_1':
-        rbd_user        => 'client.cinder',
-        rbd_pool        => 'volumes',
-        rbd_secret_uuid => '7200aea0-2ddd-4a32-aa2a-d49f66ab554c',
-      }
-    }
-    default: {
-      fail("Unsupported backend (${backend})")
-    }
-  }
-  Cinder::Type {
-      os_password     => $::openstack::keystone::admin_password,
-      os_tenant_name  => 'admin',
-      os_username     => 'admin',
-      os_auth_url     => "${::openstack::config::http_protocol}://${::openstack::config::controller_address_management}:5000/v2.0",
-  }
-  cinder::type { 'BACKEND_1':
-	set_key => 'volume_backend_name',
-	set_value => 'BACKEND_1',
-  }
-  class { 'cinder::backends':
-	enabled_backends => ['BACKEND_1'],
-  }
-  Class['Cinder::Backends'] -> Service['httpd']
-
   class { 'cinder::ceilometer': }
 }
