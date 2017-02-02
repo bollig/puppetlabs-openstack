4d42cc250529d336b234ba799e1815d4c5e5826c
Evan Bollig <bollig@gmail.com>
2017-02-02 15:28:02 -0600

    added support for grafana queries to gnocchi with non-primary controller

--- manifests/profile/ceilometer/gnocchi_auth.pp
+++ manifests/profile/ceilometer/gnocchi_auth.pp
@@ -2,6 +2,8 @@
 # For co-located api and worker nodes this appear
 # after openstack::profile::ceilometer::agent
 class openstack::profile::ceilometer::gnocchi_auth (
+  $enable_grafana_support = false,
+  $cors_allowed_origin = 'http://localhost:3000',
 ) {
 
     include ::openstack::common::gnocchi
@@ -23,4 +25,12 @@ class openstack::profile::ceilometer::gnocchi_auth (
         region       => $::openstack::config::region,
     }
 
+    if $enable_grafana_support { 
+      keystone_config { 
+        "cors/allowed_origin": value => $cors_allowed_origin;
+        # Append origin to headers to enable Safari support
+        "cors/allow_headers": value => "X-Auth-Token,X-Openstack-Request-Id,X-Subject-Token,X-Project-Id,X-Project-Name,X-Project-Domain-Id,X-Project-Domain-Name,X-Domain-Id,X-Domain-Name,origin";
+      }
+    }
+ 
 }
