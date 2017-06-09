99f2fa1982d4db9bf3a91e3eb9312520b1cd9ee1
Evan Bollig <bollig@gmail.com>
2017-06-08 19:01:02 -0500

    update heat for proxy header parsing, refactor haproxy, add http headers

--- manifests/profile/haproxy/neutron.pp
+++ manifests/profile/haproxy/neutron.pp
@@ -2,21 +2,9 @@ class openstack::profile::haproxy::neutron (
   # network nodes have the mian ip on br-ex interface rather than "default"
   $bind_address = pick($::ipaddress_br_ex, $::ipaddress),
 ) {
-  include openstack::profile::haproxy::init
 
-  haproxy::listen { 'neutron-api-in':
-    bind => {
-      # binding to a specific address, so the underlying service can bind to same port on localhost
-      "$bind_address:9696"	=> ['ssl', 'crt', $::openstack::config::haproxy_ssl_certfile],
-    },
-    require => Class['openstack::profile::haproxy::init'],
+  openstack::profile::haproxy::listen { 'neutron-api':
+    port => 9696,
   }
 
-  haproxy::balancermember { 'neutron-api-01':
-    listening_service => 'neutron-api-in',
-    ipaddresses => '127.0.0.1',
-    ports     => 9696,
-    options   => '',
-    require => Class['openstack::profile::haproxy::init'],
-  }
 }
