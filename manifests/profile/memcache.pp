bb562efe8516c33effc269b795cb83281b5d940e
Graham Allan <gta@umn.edu>
2018-03-30 18:13:38 -0500

    newer memcached module demands integer ports not strings

--- manifests/profile/memcache.pp
+++ manifests/profile/memcache.pp
@@ -10,7 +10,7 @@ class openstack::profile::memcache (
   class { 'memcached':
     listen_ip => $l_listen_ip,
     #listen_ip => $::openstack::config::controller_address_management, #'127.0.0.1',
-    tcp_port  => '11211',
-    udp_port  => '11211',
+    tcp_port  => 11211,
+    udp_port  => 11211,
   }
 }
